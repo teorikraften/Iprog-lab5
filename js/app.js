e9284b930e4c76586f63753ff8d43fc9a25e8b67
Onur Aktas <onuraktas@msn.com>
2017-02-08 16:22:36 +0100

    app.js changes
    new sofisticated changes

--- js/app.js
+++ js/app.js
@@ -4,5 +4,7 @@ $(function() {
 	
 	//And create the needed controllers and views
 	var exampleView = new ExampleView($("#exampleView"), model);
+	var selectDishView = new SelectDishView($("#container"), model);
+	var selectDishSidebarView = new SelectDishSidebarView($("#container"), model);
 
 });
