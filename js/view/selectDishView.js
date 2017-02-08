803c0d82a770544243ac5068c3c979507adb94c8
Adrian Lökk <adrian.lokk94@gmail.com>
2017-02-08 17:31:32 +0100

    polish

--- js/view/selectDishView.js
+++ js/view/selectDishView.js
@@ -3,18 +3,28 @@ var SelectDishView = function (container, model) {
 	// Get all the relevant elements of the view (ones that show data
   	// and/or ones that responed to interaction)
   	this.dishes = container.find("#dishes");
-  	var allDishes = model.getAllDishes('main dish');
- 	this.dishes.append("<div class='row'>");
-	for(var i = 0; i < allDishes.length; i++) {
+
+  	var $main = $('<div/>').addClass('row');
+  	var $starter = $('<div/>').addClass('row');
+  	var $dessert = $('<div/>').addClass('row');
+  	this.dishes.append($main);
+  	this.dishes.append($starter);
+  	this.dishes.append($dessert);
+
+
+
+  	// main dishes
+  	var mainDishes = model.getAllDishes('main dish');
+	for(var i = 0; i < mainDishes.length; i++) {
 		var $column = $('<div/>').addClass("col-md-3");
 		var $thumbnail = $('<div/>').addClass('thumbnail');
 
-		var $img = $('<img/>').attr('src', 'images/' + allDishes[i].image);
+		var $img = $('<img/>').attr('src', 'images/' + mainDishes[i].image);
 		$img.attr('class','img-rounded img-thumbnail');
 		$img.attr('style','width:100%; height:100%');
 		var $caption = $('<div/>').addClass('caption');
 
-		var $h3 = $('<h3/>').html("<a href='dishdetails.html?id=" + allDishes[i].id + "'>" + allDishes[i].name + "</a>");
+		var $h3 = $('<h3/>').html("<a href='dishdetails.html?id=" + mainDishes[i].id + "'>" + mainDishes[i].name + "</a>");
 		//var $p = $('<p/>').html(allDishes[i].description);
 
 
@@ -25,8 +35,55 @@ var SelectDishView = function (container, model) {
 		$thumbnail.append($caption);
 
 		$column.append($thumbnail);
-		$(this.dishes).append($column);
+		$main.append($column);
 	}
-	this.dishes.append("</div>");
+
+	var starterDishes = model.getAllDishes('starter');
+	for(var i = 0; i < starterDishes.length; i++) {
+		var $column = $('<div/>').addClass("col-md-3");
+		var $thumbnail = $('<div/>').addClass('thumbnail');
+
+		var $img = $('<img/>').attr('src', 'images/' + starterDishes[i].image);
+		$img.attr('class','img-rounded img-thumbnail');
+		$img.attr('style','width:100%; height:100%');
+		var $caption = $('<div/>').addClass('caption');
+
+		var $h3 = $('<h3/>').html("<a href='dishdetails.html?id=" + starterDishes[i].id + "'>" + starterDishes[i].name + "</a>");
+		//var $p = $('<p/>').html(allDishes[i].description);
+
+
+		$caption.append($h3);
+		//$caption.append($p);
+
+		$thumbnail.append($img);
+		$thumbnail.append($caption);
+
+		$column.append($thumbnail);
+		$starter.append($column);
+	}
+
+	var desserts = model.getAllDishes('dessert');
+	for(var i = 0; i < desserts.length; i++) {
+		var $column = $('<div/>').addClass("col-md-3");
+		var $thumbnail = $('<div/>').addClass('thumbnail');
+
+		var $img = $('<img/>').attr('src', 'images/' + desserts[i].image);
+		$img.attr('class','img-rounded img-thumbnail');
+		$img.attr('style','width:100%; height:100%');
+		var $caption = $('<div/>').addClass('caption');
+
+		var $h3 = $('<h3/>').html("<a href='dishdetails.html?id=" + desserts[i].id + "'>" + desserts[i].name + "</a>");
+		//var $p = $('<p/>').html(allDishes[i].description);
+
+
+		$caption.append($h3);
+		//$caption.append($p);
+
+		$thumbnail.append($img);
+		$thumbnail.append($caption);
+
+		$column.append($thumbnail);
+		$dessert.append($column);
+	}
 }
  
