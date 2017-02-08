95393c489d58426a23be09162f5839cce4a80e15
Adrian Lökk <adrian.lokk94@gmail.com>
2017-02-08 16:24:43 +0100

    -exampleview

--- js/app.js
+++ js/app.js
@@ -3,7 +3,6 @@ $(function() {
 	var model = new DinnerModel();
 	
 	//And create the needed controllers and views
-	var exampleView = new ExampleView($("#exampleView"), model);
 	var selectDishView = new SelectDishView($("#container"), model);
 	var selectDishSidebarView = new SelectDishSidebarView($("#container"), model);
 	var dishInformationView = new DishInformationView($("#dishdetails"), model);
