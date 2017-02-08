f1bb759e2b0859b32c521bc09d0cdb6cd5c75fdd
Adrian Lökk <adrian.lokk94@gmail.com>
2017-02-08 16:53:03 +0100

    grejs

--- js/view/selectDishView.js
+++ js/view/selectDishView.js
@@ -4,19 +4,22 @@ var SelectDishView = function (container, model) {
   	// and/or ones that responed to interaction)
   	this.dishes = container.find("#dishes");
   	var allDishes = model.getAllDishes('main dish');
+ 	this.dishes.append("<div class='row'>");
 	for(var i = 0; i < allDishes.length; i++) {
 		var $column = $('<div/>').addClass("col-md-3");
 		var $thumbnail = $('<div/>').addClass('thumbnail');
 
 		var $img = $('<img/>').attr('src', 'images/' + allDishes[i].image);
+		$img.attr('class','img-rounded img-thumbnail');
+		$img.attr('style','width:100%; height:100%');
 		var $caption = $('<div/>').addClass('caption');
 
 		var $h3 = $('<h3/>').html("<a href='dishdetails.html?id=" + allDishes[i].id + "'>" + allDishes[i].name + "</a>");
-		var $p = $('<p/>').html(allDishes[i].description);
+		//var $p = $('<p/>').html(allDishes[i].description);
 
 
 		$caption.append($h3);
-		$caption.append($p);
+		//$caption.append($p);
 
 		$thumbnail.append($img);
 		$thumbnail.append($caption);
@@ -24,5 +27,6 @@ var SelectDishView = function (container, model) {
 		$column.append($thumbnail);
 		$(this.dishes).append($column);
 	}
+	this.dishes.append("</div>");
 }
  
