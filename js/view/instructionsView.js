0a07355790a7ae2e1dbc546d14e42812d594d471
Onur Aktas <onura@kth.se>
2017-02-16 16:45:57 +0100

    Did it work?

--- js/view/instructionsView.js
+++ js/view/instructionsView.js
@@ -5,7 +5,34 @@ var InstructionsView = function (container, model) {
 
 	model.addObserver(this);
 	this.update = function(obj) {
+		guests = model.getNumberOfGuests();
+		container.find('#numberOfGuests').html("My Dinner: " + guests);
 		
+		$panelbody.empty();
+		menu.forEach(function(dish) {
+			$media = $("<div/>").addClass("media");
+		
+			$medialeft = $("<div/>").addClass("media-left pull-left");
+			$image = $("<img>").attr("src","images/" + dish.image);
+			$image.addClass("media-object");
+			$medialeft.append($image);
+
+			$mediabody = $("<div/>").addClass("media-body");
+			$h4 = $("<h3>").addClass("media-heading");
+			$h4.append(dish.name);
+			$mediabody.append($h4);
+			$p = $("<p/>").append(dish.description);
+			$mediabody.append($p);
+
+			$media.append($medialeft);
+			$media.append($mediabody);
+
+			$panelbody.append($media);
+			$panelbody.append($("<hr/>"));
+		});
+
+		// remove last hr tag
+		$panelbody.children().last().remove();
 	}
 
 	var body = container.find("#instructions");
@@ -16,13 +43,13 @@ var InstructionsView = function (container, model) {
 
 	$panel = $("<div/>").addClass("panel panel-default");
 	$panelheading = $("<div/>").addClass("panel-heading");
-	$panelheadingButton = $("<button/>").attr({
+	this.$panelheadingButton = $("<button/>").attr({
 		type: "button",
 		class: "btn btn-primary btn-md pull-right",
 		style: "margin-top: 10px"
 	}).html("Go back and edit dinner");
-	$panelheading.append($panelheadingButton);
-	$panelheading.append("<h3>My dinner: " + guests + "</h3>");
+	$panelheading.append(this.$panelheadingButton);
+	$panelheading.append("<h3 id = 'numberOfGuests'>My Dinner: " + guests + "</h3>");
 
 	$panelbody = $("<div/>").addClass("panel-body");
 
