c27b823dc7e729e7bae846cb67482c98ee854144
Adrian Lokk <lokk@kth.se>
2017-02-24 16:47:08 +0100

    informationcontroller

--- js/controller/dishInformationViewController.js
+++ js/controller/dishInformationViewController.js
@@ -1,17 +1,6 @@
 // dishInformationViewController
 var DishInformationViewController = function(view, model, stateController) {
 
-	$(document).on({
-		ajaxStart: function() { 
-			view.$panelBigBody.children().hide();
-			view.$loading.show(); 
-		},
-		ajaxStop: function() { 
-			view.$panelBigBody.children().show(); 
-			view.$loading.hide();
-		}
-	});
-
 	view.$button.on('click', function() {
 		model.addDishToMenu(view.dish);
 		$('<div><h4>Added <b>' + view.dishName + '</b> to menu!</h4></div>').addClass("alert alert-success text-center").insertBefore('.media').delay(3000).fadeOut();
