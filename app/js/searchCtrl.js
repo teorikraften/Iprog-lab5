0586fc60e70ce421db3fe53e602bce37e5b19af9
Adrian Lökk <adrian.lokk94@gmail.com>
2017-02-27 19:31:25 +0100

    home och selectDish (search) klar, lite gjort på dishinfo (dish)

--- app/js/searchCtrl.js
+++ app/js/searchCtrl.js
@@ -4,5 +4,17 @@ dinnerPlannerApp.controller('SearchCtrl', function ($scope,Dinner) {
 
   // TODO in Lab 5: you will need to implement a method that searchers for dishes
   // including the case while the search is still running.
+   $scope.search = function(query,type) {
+	   	$scope.status = "Searching...";
+	   	Dinner.DishSearch.get({query:query,type:type}, function(data) {
+	   		$scope.baseUri = data.baseUri;
+	     	$scope.dishes = data.results;
+	     	$scope.status = "";
+	   	}, function(data) {
+	     	$scope.status = "There was an error";
+	   	});
+	}
+
+	$scope.search();
 
 });
